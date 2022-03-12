ab0ad828ce0184ef07948c41e711a14abf63e2c6
Ivan <fenomen_ny@mail.ru>
2022-03-12 19:01:26 +0300

    Styled Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { Component, Fragment } from 'react';
+import { Component} from 'react';
+import styled from 'styled-components';
 // import { Component, Fragment } from 'react'; // ! 1 способ использования Fragment
 
 import './App.css';
 
+// styled components
+const EmpItem = styled.div`
+	padding: 20px;
+	margin-bottom: 15px;
+	border-radius: 5px;
+	background-color: #fff;
+	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`;
+
+// styled components
+const Header = styled.h2`
+	font-size: 22px;
+	color: indigo;
+`;
+
+const Button = styled.button`
+	display: block;
+	padding: 5px 15px;
+	background-color: gold;
+	border: 1px solid rgba(0,0,0, .2);
+	box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+	margin: 0 auto;
+`;
 
 // ? Состояния формат класса
-
 class WhoAmI extends Component {
 	constructor(props) { // ! нужно передать конструктор и аргументом пропс
 		super(props); // ? ключевое слово супер. Если кроме этой строки ничего нет в конструкторе, то его можно удалить
@@ -39,31 +62,40 @@ class WhoAmI extends Component {
 		const {position, years, text} = this.state;
 
 		return (
-			// ? 1й способ <Fragment></Fragment> вместо оберточного дива
-			// ? 2й способ <></> вместо оберточного дива
-			<> 
-				<button onClick={this.nextYear}>{text}</button>
-				<h1>My name is {name}, surname - {surname}, 
+			<EmpItem> 
+				<Button onClick={this.nextYear}>{text}</Button>
+				<Header>My name is {name}, surname - {surname}, 
 					age - {years}, 
-					position - {position} </h1>
+					position - {position} 
+				</Header>
 				<a href={link}>My Site</a>
 				<form>
 					<span>Введите должность</span>
 					<input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
 				</form>
-			</>
+			</EmpItem>
 		)
 	}
 }
 // ? 1й способ <Fragment> есть смысл применять когда надо задать key и др. атрибуты
 // React.Fragment key='222'
 
+// задаем styled component
+const Wrapper = styled.div`
+	width: 800px;
+	margin: 80px auto 0;
+	text-align: center;
+`;
+
 function App() {
   return (
-	<div className='App'> 
+		// используем styled component (можно с уже сущ классом) <Wrapper className='App'>
+		<Wrapper>
+		{/* <div className='App'>  */}
 		<WhoAmI name='Fenomen' surname="Russu" outname="I" link="https://ivanrussui.ru/"/>
 		<WhoAmI name='Univers' surname="Ivanova" outname="I" link="https://ivanrussui.github.io/glob-opt/dist/"/>
-	</div>
+	{/* </div> */}
+	</Wrapper>
   );
 }
 
