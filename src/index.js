0094ea5132086e618242743b2f7f467079f68917
Ivan <fenomen_ny@mail.ru>
2022-03-14 00:23:18 +0300

    react bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App, { Button } from './App';
 import styled from 'styled-components';
+import BootstrapTest from './BootstrapTest';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const BigButton = styled(Button)`
 	width: 250px;
@@ -14,6 +16,7 @@ ReactDOM.render(
   <StrictMode>
 		<App/>
 		<BigButton as="a">Отправить</BigButton>
+		<BootstrapTest/>
 	</StrictMode>,
   document.getElementById('root')
 );
